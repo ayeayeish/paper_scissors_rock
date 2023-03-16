acf9dc38d28135bf1d2bfc4b6db0d43203e761b9
ayeayeish <treasure.samuel1@gmail.com>
2023-03-17 12:48:40 +1300

    Added function with PSR rules - to play a game out of five and display results

--- p_s_r_script.js
+++ p_s_r_script.js
@@ -9,3 +9,41 @@ function getComputerChoice() {
         return "SCISSORS";
     }
 }
+
+
+function gameOutOfFive() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+
+    let playerSelection = prompt("Pick PAPER, SCISSORS, or ROCK");
+    let computerSelection = getComputerChoice();
+
+        
+    if (playerSelection === computerSelection) {
+        alert("You tied!");
+    }
+
+    if (
+        (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+        (playerSelection === "PAPER" && computerSelection === "ROCK") 
+    ) {
+        alert("You are the winner!");
+        playerScore++;
+    } 
+
+    if (
+        (playerSelection === "SCISSORS" && computerSelection === "ROCK") ||
+        (playerSelection === "PAPER" && computerSelection === "SCISSORS") ||
+        (playerSelection === "ROCK" && computerSelection === "PAPER") 
+     ) {
+        alert("You are the loser!");
+        computerScore++;
+    } 
+
+}
+
+    console.log("Your Score is : " + playerScore + ". Your opponents score is " + computerScore);
+}
